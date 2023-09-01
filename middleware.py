6e672934c0e118cfe05b4cb62786bc8f8ce9cbef
vulcan_floyd <shivendrapsingh72@gmail.com>
2023-09-01 13:17:15 +0530

    update middleware

--- middleware.py
+++ middleware.py
@@ -8,10 +8,9 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        # jwt is passed in the request header
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
-        # return 401 if token is not passed
+            
         if not token:
             return jsonify({'message' : 'Token is missing !!'}), 401
 
@@ -21,11 +20,11 @@ def token_required(f):
             # decoding the payload to fetch the stored details
             data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
             current_user = User.query.filter_by(public_id = data['public_id']).first()
+            g.user_id = current_user.id
         except:
             return jsonify({
                 'message' : 'Token is invalid !!'
             }), 401
-        # returns the current logged in users context to the routes
-        return  f(current_user, *args, **kwargs)
+        return  f(*args, **kwargs)
   
     return decorated
