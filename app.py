33249f50785a824effb1ef5be19807586b8ec479
vulcan_floyd <shivendrapsingh72@gmail.com>
2023-09-01 13:18:55 +0530

    update app

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from utils.flask_utils import flaskrun
 from main import create_app, init_api
 import os
 
+application = create_app()
 # application = create_app(os.environ['CONFIG_NAME'])
-application = create_app(os.environ['CONFIG_NAME'])
 application = init_api(application)
 
 if __name__ == "__main__":
     flaskrun(application)
-    # application.run()
